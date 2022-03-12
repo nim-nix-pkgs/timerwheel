a27e0553e1311738e964ba10ddd3a5a01980ad71
hugosenari <hugosenari@gmail.com>
2022-03-12 03:37:39 -0300

    chore: re index timerwheel

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''A high performance timer based on timerwheel for Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."timerwheel-master".url = "path:./master";
+  inputs."timerwheel-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."timerwheel-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."timerwheel-v0_1_2".url = "path:./v0_1_2";
+  inputs."timerwheel-v0_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."timerwheel-v0_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ v0_1_2/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A high performance timer based on timerwheel for Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-timerwheel-v0_1_2.flake = false;
+  inputs.src-timerwheel-v0_1_2.owner = "xflywind";
+  inputs.src-timerwheel-v0_1_2.ref   = "refs/tags/v0.1.2";
+  inputs.src-timerwheel-v0_1_2.repo  = "timerwheel";
+  inputs.src-timerwheel-v0_1_2.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-timerwheel-v0_1_2"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-timerwheel-v0_1_2";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
